77ebe8dae6cf4918f532ce1b20673a030649d92a
Hugo Tempez <77447186+hugotempez@users.noreply.github.com>
2024-03-15 14:22:01 +0100

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,10 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
-# mongo_uri = "mongodb+srv://cdesavis:ZnGa4pkR9L0YJt1t@cluster0.wdhqpjz.mongodb.net/?retryWrites=true&w=majority"
+# mongo_uri = ""
 # client = AsyncIOMotorClient(mongo_uri)
 # database = client["EPlantCare"]
-
-mongo_uri = "mongodb+srv://hugo9480:Eden%4080850@cluster0.mrxxmhh.mongodb.net/"
-client = AsyncIOMotorClient(mongo_uri)
-database = client["PLANTPILOT"]
